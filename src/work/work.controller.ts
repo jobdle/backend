3b4496c148401c51a75e33aee678a1fe85e9244f
Thanaphol Ovatvoravarunyou <thanaphol.ovat@gmail.com>
2023-02-19 22:29:38 +0700

    dbug

--- src/work/work.controller.ts
+++ src/work/work.controller.ts
@@ -31,18 +31,8 @@ export class WorkController {
     @Query('sort') sort: string,
     @Query('order') order: string,
   ): Promise<any> {
-    const a = await {
-      id: '63a0182e815d5957dbb0bae0',
-      firstname: 'admin',
-      lastname: 'admin',
-      username: 'admin',
-      password: '$2b$10$DtPj6yZ7ZHDcriC9Xi63GeuNJNTQEjdTLLS//ObNufXiXIB50OnJa',
-      role: 'admin',
-      email: 'admin@gg.com',
-    };
     return await this.workService.findAll(
-      //req.user,
-      a,
+      req.user,
       status,
       page,
       customerId,
